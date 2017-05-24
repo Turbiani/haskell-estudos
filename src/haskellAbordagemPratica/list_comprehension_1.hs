587e51aef21144e92cc8a12ae4829563eb4e83ff
Turbiani <leo.turbiani@gmail.com>
2017-05-24 19:19:56 -0300

    Ajustes

--- src/haskellAbordagemPratica/list_comprehension_1.hs
+++ src/haskellAbordagemPratica/list_comprehension_1.hs
@@ -3,4 +3,5 @@
 --A compreensão de lista que podemos usar é [x*2 | x <- [1..10]].
 --x é traçado a partir de [1..10] e para cada elemento em [1..10] (que temos vinculado a x) teremos esse elemento dobrado.
 -- Aqui esta a compreensão em ação.
+module ListComprehension_1 where
 compreensao_10_primeiros = [x*2 | x <- [1..10]]
