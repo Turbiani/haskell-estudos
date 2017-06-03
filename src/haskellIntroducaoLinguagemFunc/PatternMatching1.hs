d352a04b790d0f43d04d72ec1149a87e4b86e17d
Turbiani <leo.turbiani@gmail.com>
2017-06-03 18:05:11 -0300

    Seção 3 - Aplicando Pattern Matching em tuplas - 2

--- src/haskellIntroducaoLinguagemFunc/PatternMatching1.hs
+++ src/haskellIntroducaoLinguagemFunc/PatternMatching1.hs
@@ -7,6 +7,11 @@ f (x,0) = x -- valor variável na primeira e fixo na segunda
 f (0,y) = y -- valor fixo na primeira e variável na segunda
 f (x,y) = x+y -- valores variáveis em ambas as coordenadas (devem possuir nomes diferentes)
 
-
-
+g :: (Int, Int) -> Int
+g (7,7) = 7
+--g (_,_) = 10
+-- quando usamos _ além dos valores serem variáveis, eles são ignorados
+-- ou seja, na definição abaixo, eu ignoro qualquer valor passado para g
+-- e comportamento da função é o mesmo de quando eu declarei g (_,_) = 10
+g _ = 10
 
