8df29c46cbc417dcc262631827dbccf737be889b
Turbiani <leo.turbiani@gmail.com>
2017-06-08 20:03:06 -0300

    Seção 3.2 - Campos de um Construtor - Usando PatternMatching

--- src/haskellIntroducaoLinguagemFunc/PatternMatchingListas.hs
+++ src/haskellIntroducaoLinguagemFunc/PatternMatchingListas.hs
@@ -5,6 +5,6 @@ h (_:[]) = 1
 h (_:x:[]) = 2+x
 h (x:y:z:[]) = 3+x+y+z
 h (x:_:_:w:[]) = 4+x+w
---h (x:xs) = x -- Podeos ler que aqui eu pego um array com 1 ou mais elementos(que nao caia nos outros matchings)
--- ou (Mas perco a referência de x)
+--h (x:xs) = x -- Podemos ver que aqui eu pego um array com 1 ou mais elementos(que nao caia nos outros matchings)
+-- ou (Mas perco a referência de x) posso usar o _ que ignora qualquer valor passado para h
 h (_) = 11
---
+++ src/haskellIntroducaoLinguagemFunc/PatternMatchingValueConstructor.hs
@@ -0,0 +1,11 @@
+module PatternMatchingValueConstructor where
+data Pessoa = Fisica String Int | Juridica String
+teste :: Pessoa -> (String, String)
+teste (Fisica x y) = ("Nome: " ++ x, "Idade: " ++ show y)
+teste (Juridica x) = ("Nome: " ++ x, "Nao ha idade")
+
+-- Nesse exemplo, o pattern matching se da que quando chamo teste  ele entende
+-- qual comportamento deve realizar para dar uma tupla de output como determinado pela assinatura
+-- teste :: Pessoa -> (String, String)
+-- E nesse entendimento, ele também infere que x é String e y é int...Por isso precisei dar show em y
+-- Exemplo de chamada teste (Fisica "Leonardo" 10) ou
