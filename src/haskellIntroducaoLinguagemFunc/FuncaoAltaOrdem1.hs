4658bdcbd6246777e5bbcde0d84d131152db294b
Turbiani <leo.turbiani@gmail.com>
2017-08-18 15:44:40 -0300

    Seção 4 do Livro - Lambdas e High Order Functions

---
+++ src/haskellIntroducaoLinguagemFunc/FuncaoAltaOrdem1.hs
@@ -0,0 +1,9 @@
+module FuncaoAltaOrdem1 where
+ev :: (Int -> Int) -> Int
+ev f = 1 + f 5
+
+dobro :: Int -> Int
+dobro x = 2*x
+
+triplo :: Int -> Int
+triplo x = 3*x
---
+++ src/haskellIntroducaoLinguagemFunc/PrimeiroLambda.hs
@@ -0,0 +1,4 @@
+module PrimeiroLambda where
+(\x -> x*2) 4
+
+(\x xs -> x : reverse xs) 'A' "UOIE"
